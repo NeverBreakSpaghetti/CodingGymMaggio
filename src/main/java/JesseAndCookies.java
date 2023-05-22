0777b82f11c33daccc664ff8f5ad22807be76133
mauro <neverbreakspaghetti@protonmail.com>
2023-05-22 21:54:59 +0200

    g

--- src/main/java/JesseAndCookies.java
+++ src/main/java/JesseAndCookies.java
@@ -31,44 +31,44 @@ public class JesseAndCookies {
 
         cookiesQueue.add(mixedCookie);
     }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static int cookies(int sweetness, List<Integer> input) {
+        List<Integer> cookieArrayList = new ArrayList<>(input); // used to have the implementation of remove(int index)
+        int iterations = 0;
+        Collections.sort(cookieArrayList);
 
-//    public static int cookies(int sweetness, List<Integer> input) {
-//        List<Integer> cookieArrayList = new ArrayList<>(input); // used to have the implementation of remove(int index)
-//        int iterations = 0;
-//        Collections.sort(cookieArrayList);
-//
-//        while (existCookieBelowSweetness(sweetness, cookieArrayList) && cookieArrayList.size()>1){
-//            mix2Cookies(cookieArrayList);
-//            iterations++;
-//        }
-//
-//        if (cookieArrayList.get(0)<sweetness)
-//            return -1;
-//
-//        return iterations;
-//    }
-//
-//    private static boolean existCookieBelowSweetness(int sweetness, List<Integer> cookieList) {
-//        return cookieList.get(0) < sweetness;
-//    }
-//
-//    private static void mix2Cookies(List<Integer> cookiesList) {
-//        int leastSweetCookie = cookiesList.remove(0);
-//        int secondLeastSweetCookie = cookiesList.remove(0);
-//
-//        int mixedCookie = leastSweetCookie + 2*secondLeastSweetCookie;
-//
-//        insertNewCookieInTheRightPosition(cookiesList, mixedCookie);
-//    }
-//
-//    private static void insertNewCookieInTheRightPosition(List<Integer> cookiesList, int cookie) {
-//        int rightPosition = Collections.binarySearch(cookiesList, cookie);
-//
-//        if (rightPosition<0) {
-//            rightPosition *= -1;
-//            rightPosition -= 1;
-//        }
-//
-//        cookiesList.add(rightPosition,cookie);
-//    }
+        while (existCookieBelowSweetness(sweetness, cookieArrayList) && cookieArrayList.size()>1){
+            mix2Cookies(cookieArrayList);
+            iterations++;
+        }
+
+        if (cookieArrayList.get(0)<sweetness)
+            return -1;
+
+        return iterations;
+    }
+
+    private static boolean existCookieBelowSweetness(int sweetness, List<Integer> cookieList) {
+        return cookieList.get(0) < sweetness;
+    }
+
+    private static void mix2Cookies(List<Integer> cookiesList) {
+        int leastSweetCookie = cookiesList.remove(0);
+        int secondLeastSweetCookie = cookiesList.remove(0);
+
+        int mixedCookie = leastSweetCookie + 2*secondLeastSweetCookie;
+
+        insertNewCookieInTheRightPosition(cookiesList, mixedCookie);
+    }
+
+    private static void insertNewCookieInTheRightPosition(List<Integer> cookiesList, int cookie) {
+        int rightPosition = Collections.binarySearch(cookiesList, cookie);
+
+        if (rightPosition<0) {
+            rightPosition *= -1;
+            rightPosition -= 1;
+        }
+
+        cookiesList.add(rightPosition,cookie);
+    }
 }
